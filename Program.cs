225e800c2d2d06c28522428e5227f81aac1510ea
Eduardo <eduardomendoncabraga@gmail.com>
2021-10-13 14:33:38 -0300

    Adding subtraction function

--- Program.cs
+++ Program.cs
@@ -6,20 +6,41 @@ namespace Calculator
   {
     static void Main(string[] args)
     {
-      Soma();
+      //Soma();
+      Subtracao();
     }
 
     static void Soma()
     {
       Console.Clear();
+
       Console.WriteLine("Primeio valor: ");
       float v1 = float.Parse(Console.ReadLine());
+
       Console.WriteLine("Segundo valor: ");
       float v2 = float.Parse(Console.ReadLine());
 
-      float resultado = v1 + v2;
       Console.WriteLine("");
+
+      float resultado = v1 + v2;
       Console.WriteLine($"O resultado da soma é {resultado} ");
+      Console.ReadKey();
+    }
+    static void Subtracao()
+    {
+      Console.Clear();
+
+      Console.WriteLine("Primeio valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      Console.WriteLine("");
+
+      float resultado = v1 - v2;
+      Console.WriteLine($"O resultado da subtração é {resultado} ");
+      Console.ReadKey();
     }
   }
 }
