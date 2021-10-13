17fa3f32044e567bd46c4b043f487300dd837b8e
Eduardo <eduardomendoncabraga@gmail.com>
2021-10-13 14:27:09 -0300

    Create soma

--- Program.cs
+++ Program.cs
@@ -2,11 +2,19 @@
 
 namespace Calculator
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      Console.Clear();
+      Console.WriteLine("Primeio valor: ");
+      float v1 = float.Parse(Console.ReadLine());
+      Console.WriteLine("Segundo valor: ");
+      float v2 = float.Parse(Console.ReadLine());
+
+      float resultado = v1 + v2;
+      Console.WriteLine("");
+      Console.WriteLine($"O resultado da soma é {resultado} ");
     }
+  }
 }
