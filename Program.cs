f2fa0332264b0a40452ea36fc21e33cc43e60e7f
Eduardo <eduardomendoncabraga@gmail.com>
2021-10-13 14:56:10 -0300

    Run options in function menu

--- Program.cs
+++ Program.cs
@@ -23,6 +23,15 @@ namespace Calculator
       Console.WriteLine("Selecione uma opção: ");
       short res = short.Parse(Console.ReadLine());
 
+      switch (res)
+      {
+        case 1: Soma(); break;
+        case 2: Subtracao(); break;
+        case 3: Divisao(); break;
+        case 4: Multiplicacao(); break;
+
+        default: Menu(); break;
+      }
     }
 
     static void Soma()
@@ -40,6 +49,7 @@ namespace Calculator
       float resultado = v1 + v2;
       Console.WriteLine($"O resultado da soma é {resultado} ");
       Console.ReadKey();
+      Menu();
     }
     static void Subtracao()
     {
@@ -56,6 +66,7 @@ namespace Calculator
       float resultado = v1 - v2;
       Console.WriteLine($"O resultado da subtração é {resultado} ");
       Console.ReadKey();
+      Menu();
     }
     static void Divisao()
     {
@@ -72,6 +83,7 @@ namespace Calculator
       float resultado = v1 / v2;
       Console.WriteLine($"O resultado da divisao é {resultado} ");
       Console.ReadKey();
+      Menu();
     }
     static void Multiplicacao()
     {
@@ -88,6 +100,7 @@ namespace Calculator
       float resultado = (v1 * v2);
       Console.WriteLine($"O resultado da multiplicação é {resultado} ");
       Console.ReadKey();
+      Menu();
     }
   }
 }
